243ef5ffde46e1f4038dc09c02deb1e24752ece7
Kevin De Gouveia <degouveiakevin@gmail.com>
2024-11-25 19:28:24 +0000

    WIP - Adding an Update Function to frontend.

--- frontend/get_medications.js
+++ frontend/get_medications.js
@@ -28,17 +28,23 @@ xhttpr.onload = () => {
                 price.appendChild(price_container)
 
                 let buttonCell = row.insertCell(2)
-                let button = document.createElement('button')
-                button.textContent = 'Delete Medication'
-                button.onclick = () => {
+                let delete_button = document.createElement('button')
+                let update_button = document.createElement('button')
+                delete_button.textContent = 'Delete Medication'
+                delete_button.onclick = () => {
                     delete_medication(item.name, row)
                 }
-                buttonCell.appendChild(button);
+                buttonCell.appendChild(delete_button)
+                update_button.textContent = 'Change Price'
+                update_button.onclick = () => {
+                }
+                buttonCell.appendChild(update_button)
             }
         )
     }
 };
 
+// Deletes the row from the html table and calls the delete function from the backend.
 function delete_medication(name, row) {
     if (name == null || name.len < 1) {
         return
@@ -53,9 +59,21 @@ function delete_medication(name, row) {
             const response = JSON.parse(xml.responseText);
             row.remove()
         } else {
-             const response = JSON.parse(xml.response);
-             alert(response.detail[0].msg);
+            const response = JSON.parse(xml.response);
+            alert(response.detail[0].msg);
         }
     };
     xml.send(`name=` + name);
+}
+
+// allows for the price of the medicine to be updated
+function update_price(name, new_price) {
+    if (new_price >= 0) {
+        const xml = new XMLHttpRequest();
+        xml.open('POST', 'http://localhost:8000/update', true);
+        xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    } else {
+        alert("The new price must be set to 0 or above." + new_price + " is not a valid value!")
+    }
 }
